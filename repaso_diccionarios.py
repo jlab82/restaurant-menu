260643f60787b847f2b5de1130816aad60d898f3
jlab82 <joseluis@identifica-t.com>
2017-12-08 19:14:12 +0100

    Programa que localiza al ladrón de donuts

---
+++ repaso_diccionarios.py
@@ -0,0 +1,7 @@
+some_dictionary = {"happiness": "state of feeling happy", "luck": "a random event with a positive effect"}
+
+cities_dict = {1000: "Ljubljana", 8000: "Novo mesto", 2000: "Maribor"}
+
+cars = {"clio": "renault", "m3": "bmw", "micra": "nissan", "i8": "bmw"}
+
+
